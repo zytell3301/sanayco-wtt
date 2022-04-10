1398aa864c775587a91755e9b51ea647c72f140a
zytell3301 <zytel3301@gmail.com>
2022-04-10 12:37:48 +0430

    Add EntityNotFound error

---
+++ GrpcService/Domain/Errors/EntityNotFound.cs
@@ -0,0 +1,9 @@
+﻿namespace GrpcService1.Domain.Errors;
+
+public class EntityNotFound : Status
+{
+    public EntityNotFound(string message) : base(message)
+    {
+        StatusCode = 3;
+    }
+}
