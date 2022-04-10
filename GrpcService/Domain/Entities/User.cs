410a7cfcaf3bc3ad6fb576a1674de6a2f835e0f3
zytell3301 <zytel3301@gmail.com>
2022-04-10 08:55:58 +0430

    Add domain entity type User

---
+++ GrpcService/Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcService1.Domain.Entities;
+
+public class User
+{
+    public int Id;
+    public string Name;
+    public string LastName;
+    public string SkillLevel;
+    public string CompanyLevel;
+}
