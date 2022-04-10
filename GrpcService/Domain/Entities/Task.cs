4c479e9b9db904436f2c9c0dfbf54b9bc3a3f334
zytell3301 <zytel3301@gmail.com>
2022-04-10 10:15:07 +0430

    Add Task domain entity type

---
+++ GrpcService/Domain/Entities/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace GrpcService1.Domain.Entities;
+
+public class Task
+{
+    public string TaskTitle;
+    public string ProjectId;
+    public string WorkLocation;
+    public DateTime TaskTime;
+    public DateTime TaskEndtime;
+    public string TaskDescription;
+}
