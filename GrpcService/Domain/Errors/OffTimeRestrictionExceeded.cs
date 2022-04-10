66eb5746151c42928b94bfc78bf1725ada30c119
zytell3301 <zytel3301@gmail.com>
2022-04-10 11:53:05 +0430

    Add OffTimeRestrictionExceeded status

---
+++ GrpcService/Domain/Errors/OffTimeRestrictionExceeded.cs
@@ -0,0 +1,12 @@
+﻿namespace GrpcService1.Domain.Errors;
+
+public class OffTimeRestrictionExceeded : Status
+{
+    public OffTimeRestrictionExceeded(string message) : base(message)
+    {
+        /*
+         * Status code 0 for successful call
+         */
+        this.StatusCode = 2;
+    }
+}
