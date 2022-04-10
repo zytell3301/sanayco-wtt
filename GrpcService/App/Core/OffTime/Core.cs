df7eb0fe758b96bd1e03a1e590fd20cc9238b080
zytell3301 <zytel3301@gmail.com>
2022-04-10 11:57:15 +0430

    Add RecordOffTime method to off time core

--- GrpcService/App/Core/OffTime/Core.cs
+++ GrpcService/App/Core/OffTime/Core.cs
@@ -1,4 +1,6 @@
-﻿using GrpcService1.Domain.Errors;
+﻿using Google.Protobuf.WellKnownTypes;
+using GrpcService1.Domain.Entities;
+using GrpcService1.Domain.Errors;
 
 namespace GrpcService1.App.Core.OffTime;
 
@@ -6,12 +8,22 @@ public class Core
 {
     private InternalError InternalError;
     private OperationSuccessful OperationSuccessful;
+    private OffTimeRestrictionExceeded OffTimeRestrictionExceeded;
+
     private IDatabase Database;
+
+    /*
+     * Number of seconds that if exceeded, the manager must approve the
+     * off time.
+     */
+    private int OffTimeRestriction;
 
     public class OffTimeCoreConfigs
     {
         public string OperationsuccessfulMessage;
         public string InternalErrorMessage;
+        public string OffTimeRestrictionExceededMessage;
+        public int OffTimeRestriction;
     }
 
     public class OffTimeDependencies
@@ -23,7 +35,39 @@ public class Core
     {
         InternalError = new InternalError(configs.InternalErrorMessage);
         OperationSuccessful = new OperationSuccessful(configs.OperationsuccessfulMessage);
+        OffTimeRestrictionExceeded = new OffTimeRestrictionExceeded(configs.OffTimeRestrictionExceededMessage);
+        OffTimeRestriction = configs.OffTimeRestriction;
 
         Database = dependencies.Database;
+    }
+
+    /*
+     * @TODO Add extra checking for off time availability 
+     */
+    public Status RecordOffTime(User user, Domain.Entities.OffTime offTime)
+    {
+        long totalOffTimeDuration = 0;
+        Domain.Entities.OffTime[] history = Database.GetOffTimeHistory(user, DateTime.Now.AddMonths(-1), DateTime.Now);
+        foreach (var offTimeHistory in history)
+        {
+            totalOffTimeDuration += offTimeHistory.to.ToTimestamp().Seconds - offTime.@from.ToTimestamp().Seconds;
+        }
+
+        switch (totalOffTimeDuration >= OffTimeRestriction)
+        {
+            case true:
+                return OffTimeRestrictionExceeded;
+        }
+
+        try
+        {
+            Database.RecordOffTime(offTime);
+        }
+        catch (Exception e)
+        {
+            return InternalError;
+        }
+
+        return OperationSuccessful;
     }
 }
