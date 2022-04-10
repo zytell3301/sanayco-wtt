75b3b0150a64183e9fc7897abb3c8d8de7d57568
zytell3301 <zytel3301@gmail.com>
2022-04-10 09:29:19 +0430

    Add status for internal error

---
+++ GrpcService/Domain/Errors/InternalError.cs
@@ -0,0 +1,12 @@
+﻿namespace GrpcService1.Domain.Errors;
+
+public class InternalError : Status
+{
+    public InternalError(string message) : base(message)
+    {
+        /*
+         * Status code for internal error
+         */
+        StatusCode = 1;
+    }
+}
