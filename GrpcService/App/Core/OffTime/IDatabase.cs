bd15e02d9f61d29da14187ddb8ccda37ece77ab3
zytell3301 <zytel3301@gmail.com>
2022-04-10 11:34:18 +0430

    Add GetOffTimeHistory method to IDatabase interface

--- GrpcService/App/Core/OffTime/IDatabase.cs
+++ GrpcService/App/Core/OffTime/IDatabase.cs
@@ -1,6 +1,9 @@
-﻿namespace GrpcService1.App.Core.OffTime;
+﻿using GrpcService1.Domain.Entities;
+
+namespace GrpcService1.App.Core.OffTime;
 
 public interface IDatabase
 {
     public void RecordOffTime(Domain.Entities.OffTime offTime);
+    public Domain.Entities.OffTime[]  GetOffTimeHistory(User user,DateTime from, DateTime to);
 }
