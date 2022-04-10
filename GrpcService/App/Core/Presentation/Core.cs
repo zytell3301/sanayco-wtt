c7e6ec488fc929541a87e61c6843507d80710316
zytell3301 <zytel3301@gmail.com>
2022-04-10 09:38:59 +0430

    Add RecordPresentation method in presentation core

--- GrpcService/App/Core/Presentation/Core.cs
+++ GrpcService/App/Core/Presentation/Core.cs
@@ -1,11 +1,36 @@
-﻿namespace GrpcService1.App.Core.Presentation;
+﻿using GrpcService1.Domain.Entities;
+using GrpcService1.Domain.Errors;
+
+namespace GrpcService1.App.Core.Presentation;
 
 public class Core
 {
     private IDatabase database;
+    private InternalError InternalError;
 
-    public Core(IDatabase database)
+    public class PresentationCoreConfig
+    {
+        public string InternalErrorMessage;
+    }
+
+    public Core(IDatabase database, PresentationCoreConfig config)
     {
         this.database = database;
+        /*
+         * Initiate the error instance once and use it forever.
+         */
+        InternalError = new InternalError(config.InternalErrorMessage);
+    }
+
+    public Status RecordPresentation(User user)
+    {
+        try
+        {
+            database.RecordPresentation(user);
+        }
+        catch (InternalError e)
+        {
+            return new InternalError("");
+        }
     }
 }
