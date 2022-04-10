96aee0647bd3384dbdf49e63211224597dccd9af
zytell3301 <zytel3301@gmail.com>
2022-04-10 10:02:34 +0430

    Add GetPresentationTime to IDatabase interface

--- GrpcService/App/Core/Presentation/Database.cs
+++ GrpcService/App/Core/Presentation/Database.cs
@@ -11,4 +11,11 @@ public interface IDatabase
 {
     public void RecordPresentation(User user);
     public void RecordPresentationEnd(User user);
+
+    /*
+     * Current application of this api for now is for calculating user's presentation time.
+     * If this will be the only application, you can consider adding a UserIsNotRepresent exception
+     * for the case that the user has ended his representation and currently not present.
+     */
+    public DateTime GetPresentationTime(User user);
 }
