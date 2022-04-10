712fdc1a0810750dd5bf495910da22dff554991c
zytell3301 <zytel3301@gmail.com>
2022-04-10 10:12:19 +0430

    Add core for Tasks

---
+++ GrpcService/App/Core/Tasks/Core.cs
@@ -0,0 +1,25 @@
+﻿using GrpcService1.Domain.Errors;
+
+namespace GrpcService1.App.Core.Tasks;
+
+public class Core
+{
+    private OperationSuccessful OperationSuccessful;
+    private InternalError InternalError;
+
+    public class TasksCoreConfigs
+    {
+        public string OperationSuccessfulmessage;
+        public string InternalErrorMessage;
+    }
+
+    public class TasksCoreDependencies
+    {
+    }
+
+    public Core(TasksCoreDependencies dependencies, TasksCoreConfigs configs)
+    {
+        OperationSuccessful = new OperationSuccessful(configs.OperationSuccessfulmessage);
+        InternalError = new InternalError(configs.InternalErrorMessage);
+    }
+}
