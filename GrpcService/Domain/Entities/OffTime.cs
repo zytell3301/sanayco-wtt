b605c8c60710d99ae0ff8168359c303a88bbc34b
zytell3301 <zytel3301@gmail.com>
2022-04-10 11:30:07 +0430

    Update off time domain entity

--- GrpcService/Domain/Entities/OffTime.cs
+++ GrpcService/Domain/Entities/OffTime.cs
@@ -4,11 +4,13 @@ public class OffTime
 {
     public int Id;
     public int UserId;
-    
+
     /*
      * Every off time has three statuses. Approved,Rejected and Waiting. 
      */
     public string Status;
     public DateTime Date;
+    public DateTime from;
+    public DateTime to;
     public string Description;
 }
