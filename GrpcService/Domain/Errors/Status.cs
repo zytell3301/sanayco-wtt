4a35e56dbb108148e0f3bf008edde3b5033abe7d
zytell3301 <zytel3301@gmail.com>
2022-04-10 09:18:53 +0430

    Add base status class

---
+++ GrpcService/Domain/Errors/Status.cs
@@ -0,0 +1,28 @@
+﻿namespace GrpcService1.Domain.Errors;
+
+/**
+ * This is base class for all statuses that exists cross the application.
+ * Every status represents an error message (If supplied by the creator of class).
+ * StatusCodes must be unique cross the application so the caller can exactly distinct errors.
+ * Status code 0 is reserved as successful call and 1 is reserved for internal error.
+ */
+public class Status : Exception
+{
+    protected int StatusCode;
+    protected string Message;
+
+    public Status(string message)
+    {
+        Message = message;
+    }
+
+    public int GetStatusCode()
+    {
+        return StatusCode;
+    }
+
+    public string GetMessage()
+    {
+        return Message;
+    }
+}
