e2f17cfd4d1b9de7f967449ac3f13941f01cf329
zytell3301 <zytel3301@gmail.com>
2022-04-10 09:28:01 +0430

    Add status Operationsuccessful

---
+++ GrpcService/Domain/Errors/OperationSuccessful.cs
@@ -0,0 +1,12 @@
+﻿namespace GrpcService1.Domain.Errors;
+
+public class OperationSuccessful : Status
+{
+    public OperationSuccessful(string message) : base(message)
+    {
+        /*
+         * Status code 0 for successful call
+         */
+        this.StatusCode = 0;
+    }
+}
