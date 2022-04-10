d6feb122602c21eb3146161db0aa7ad5421afb14
zytell3301 <zytel3301@gmail.com>
2022-04-10 12:09:22 +0430

    Add ChangeOffTimeStatus method to IDatabase interface

--- GrpcService/App/Core/OffTime/IDatabase.cs
+++ GrpcService/App/Core/OffTime/IDatabase.cs
@@ -5,5 +5,6 @@ namespace GrpcService1.App.Core.OffTime;
 public interface IDatabase
 {
     public void RecordOffTime(Domain.Entities.OffTime offTime);
-    public Domain.Entities.OffTime[]  GetOffTimeHistory(User user,DateTime from, DateTime to);
+    public Domain.Entities.OffTime[] GetOffTimeHistory(User user, DateTime from, DateTime to);
+    public void ChangeOffTimeStatus(Domain.Entities.OffTime offTime,string status);
 }
