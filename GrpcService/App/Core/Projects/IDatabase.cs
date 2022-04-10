72e6c5c8d98799ead8d916509d54e22d5ea265db
zytell3301 <zytel3301@gmail.com>
2022-04-10 12:46:01 +0430

    Add RemoveUserFromProject method to IDatabase interface

--- GrpcService/App/Core/Projects/IDatabase.cs
+++ GrpcService/App/Core/Projects/IDatabase.cs
@@ -7,5 +7,6 @@ public interface IDatabase
     public void RecordProject(Project project);
     public void UpdateProject(Project project);
     public void AddMemberToProject(Project project, User user);
+    public void RemoveUserFromProject(Project project, User user);
     public void DeleteProject(Project project);
 }
