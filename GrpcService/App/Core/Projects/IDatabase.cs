2d6c72a4b14bbf7b59f9808f2fe302236ddee523
zytell3301 <zytel3301@gmail.com>
2022-04-10 12:43:52 +0430

    Add DeleteProject method to IDatabase interface

--- GrpcService/App/Core/Projects/IDatabase.cs
+++ GrpcService/App/Core/Projects/IDatabase.cs
@@ -7,4 +7,5 @@ public interface IDatabase
     public void RecordProject(Project project);
     public void UpdateProject(Project project);
     public void AddMemberToProject(Project project, User user);
+    public void DeleteProject(Project project);
 }
