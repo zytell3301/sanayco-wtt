24db564df3af874b73427ddf4c1e4b6fa1ed5531
zytell3301 <zytel3301@gmail.com>
2022-04-11 15:48:20 +0430

    Implement RecordProjectBatch interface

---
+++ GrpcService/Database/Projects/RecordProjectBatch.cs
@@ -0,0 +1,49 @@
+﻿using GrpcService1.App.Core.Projects;
+using GrpcService1.Domain.Entities;
+using GrpcService1.Domain.Errors;
+using GrpcService1.ErrorReporter;
+
+namespace GrpcService1.Database.Projects;
+
+public class RecordProjectBatch : IRecordProjectBatch
+{
+    private GrpcService1.Database.Connection Connection;
+    private IErrorReporter ErrorReporter;
+
+    public RecordProjectBatch(GrpcService1.Database.Projects.Connection connection, IErrorReporter errorReporter)
+    {
+        Connection = connection;
+        ErrorReporter = errorReporter;
+    }
+
+    public void AddProjectMember(ProjectMember projectMember)
+    {
+        try
+        {
+            Connection.Add(new ProjectMember()
+            {
+                Level = projectMember.Level,
+                ProjectId = projectMember.ProjectId,
+                UserId = projectMember.UserId,
+            });
+        }
+        catch (Exception e)
+        {
+            ErrorReporter.ReportException(e);
+            throw new InternalError("");
+        }
+    }
+
+    public void ExecuteOperation()
+    {
+        try
+        {
+            Connection.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            ErrorReporter.ReportException(e);
+            throw new InternalError("");
+        }
+    }
+}
