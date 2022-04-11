0ae69cf9f70c81cb5ba0ac662ad08c27ba1a8d32
zytell3301 <zytel3301@gmail.com>
2022-04-11 14:28:25 +0430

    Add connection class for database

---
+++ GrpcService/Database/Connection.cs
@@ -0,0 +1,21 @@
+﻿using GrpcService1.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrpcService1.Database;
+
+public class Connection : DbContext
+{
+    private string ConnectionString;
+
+    public DbSet<Domain.Entities.Task> Tasks { get; set; }
+
+    public Connection(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionString);
+    }
+}
