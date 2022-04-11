ab4b11069e31e9d6e822c638106457e367173f50
zytell3301 <zytel3301@gmail.com>
2022-04-11 15:16:56 +0430

    Add separate connection for projects database class

---
+++ GrpcService/Database/Projects/Connection.cs
@@ -0,0 +1,13 @@
+﻿using GrpcService1.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrpcService1.Database.Projects;
+
+public class Connection : GrpcService1.Database.Connection
+{
+    public DbSet<Project> Projects;
+
+    public Connection(string connectionString) : base(connectionString)
+    {
+    }
+}
