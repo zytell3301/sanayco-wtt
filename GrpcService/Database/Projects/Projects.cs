5286181d20bb7ad082ba5da5d2a61ff59d3d3810
zytell3301 <zytel3301@gmail.com>
2022-04-11 15:00:43 +0430

    Add Database/Projects class as database for projects core

---
+++ GrpcService/Database/Projects/Projects.cs
@@ -0,0 +1,32 @@
+﻿using GrpcService1.App.Core.Projects;
+using GrpcService1.Domain.Entities;
+
+namespace GrpcService1.Database.Projects;
+
+public class Projects : IDatabase
+{
+    public IRecordProjectBatch RecordProject(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProject(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddMemberToProject(Project project, User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveUserFromProject(Project project, User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteProject(Project project)
+    {
+        throw new NotImplementedException();
+    }
+}
