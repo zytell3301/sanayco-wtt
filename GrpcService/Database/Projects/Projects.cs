8dfb6b3337cd81c4e54d090a5082115ddbb6a6bf
zytell3301 <zytel3301@gmail.com>
2022-04-11 15:20:05 +0430

    Load basic dependencies of projects class on construct

--- GrpcService/Database/Projects/Projects.cs
+++ GrpcService/Database/Projects/Projects.cs
@@ -1,10 +1,20 @@
 ﻿using GrpcService1.App.Core.Projects;
 using GrpcService1.Domain.Entities;
+using GrpcService1.ErrorReporter;
 
 namespace GrpcService1.Database.Projects;
 
 public class Projects : IDatabase
 {
+    private Connection Connection;
+    private IErrorReporter ErrorReporter;
+
+    public Projects(GrpcService1.Database.Projects.Connection connection, IErrorReporter errorReporter)
+    {
+        Connection = connection;
+        ErrorReporter = errorReporter;
+    }
+
     public IRecordProjectBatch RecordProject(Project project)
     {
         throw new NotImplementedException();
