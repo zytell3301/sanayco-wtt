ea26f1e372adf61e384a20c9d9646d058c21ae83
zytell3301 <zytel3301@gmail.com>
2022-04-11 11:11:11 +0430

    Add project member to domain entity

---
+++ GrpcService/Domain/Entities/ProjectMember.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcService1.Domain.Entities;
+
+public class ProjectMember
+{
+    public int Id;
+    public int UserId;
+    public int ProjectId;
+    public string MemberLevel;
+    public DateTime CreatedAt;
+}
