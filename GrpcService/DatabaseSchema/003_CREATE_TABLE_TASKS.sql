1a27c5cb31b3e927584d680e5332bbfbda8e19de
zytell3301 <zytel3301@gmail.com>
2022-04-11 10:03:13 +0430

    Add database schema for tasks table

---
+++ GrpcService/DatabaseSchema/003_CREATE_TABLE_TASKS.sql
@@ -0,0 +1,11 @@
+CREATE TABLE tasks
+(
+    id            INT,
+    title         CHAR,
+    project_id    INT,
+    work_location CHAR,
+    created_at    DATETIME,
+    end_time      DATETIME,
+    description   VARCHAR,
+    status        CHAR,
+)
