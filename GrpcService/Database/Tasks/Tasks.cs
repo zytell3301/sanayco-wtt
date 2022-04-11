82ed9deb9894b204cbde8067f4da75c20df0c021
zytell3301 <zytel3301@gmail.com>
2022-04-11 14:19:29 +0430

    Add tasks class as tasks core database

---
+++ GrpcService/Database/Tasks/Tasks.cs
@@ -0,0 +1,26 @@
+﻿using GrpcService1.App.Core.Tasks;
+
+namespace GrpcService1.Database.Tasks;
+
+public class Tasks : IDatabase
+{
+    public void RecordTask(Domain.Entities.Task task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteTask(Domain.Entities.Task task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EditTask(Domain.Entities.Task task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChangeTaskStatus(Domain.Entities.Task task, string status)
+    {
+        throw new NotImplementedException();
+    }
+}
