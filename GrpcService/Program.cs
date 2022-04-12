7f916abcd323783b885e9d62eacdd1be457dfd81
zytell3301 <zytel3301@gmail.com>
2022-04-12 12:05:53 +0430

    Update program.cs

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -2,15 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
+builder.Services.AddControllers();
 // Add services to the container.
-builder.Services.AddGrpc();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGet("/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+// app.MapGet("/",
+    // () =>
+        // "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapControllers();
 
 app.Run();
