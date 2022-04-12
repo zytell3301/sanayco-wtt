437536637ef7f8d1f3949a770b396fc5e3e2dad5
zytell3301 <zytel3301@gmail.com>
2022-04-12 12:10:21 +0430

    Add base http handler for tasks core

---
+++ GrpcService/App/Handlers/Http/Tasks/Handler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcService1.App.Handlers;
+
+public class Handler : ControllerBase
+{
+    private Core.Tasks.Core Core;
+
+    public Handler(Core.Tasks.Core core)
+    {
+        Core = core;
+    }
+}
