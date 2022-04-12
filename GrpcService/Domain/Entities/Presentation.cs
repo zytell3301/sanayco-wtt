3ff1b7f30703911af5f7e54b1283d1454906330d
zytell3301 <zytel3301@gmail.com>
2022-04-12 11:00:06 +0430

    Add presentation domain entity

---
+++ GrpcService/Domain/Entities/Presentation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GrpcService1.Domain.Entities;
+
+[Table("presentations", Schema = "wtt")]
+public class Presentation
+{
+    [Column("id")] public int Id;
+    [Column("user_id")] public int UserId;
+    [Column("start")] public DateTime Start;
+    [Column("end")] public DateTime End;
+}
