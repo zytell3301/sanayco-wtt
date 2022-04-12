9836c7e2427c90d3f2105fc1ff29d5f338e1a172
zytell3301 <zytel3301@gmail.com>
2022-04-12 13:15:00 +0430

    Load tasks core into service container

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -1,15 +1,19 @@
+using GrpcService1.App.Core.Tasks;
+using GrpcService1.Database.Tasks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 builder.Services.AddControllers();
+builder.Services.AddSingleton<Core>(new Core(new Core.TasksCoreDependencies(), new Core.TasksCoreConfigs()));
 // Add services to the container.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 // app.MapGet("/",
-    // () =>
-        // "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+// () =>
+// "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 app.MapControllers();
 
 app.Run();
