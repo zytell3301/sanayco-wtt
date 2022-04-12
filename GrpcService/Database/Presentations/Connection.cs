5b637ac8d53acf899303c4a278727254b509e954
zytell3301 <zytel3301@gmail.com>
2022-04-12 11:10:27 +0430

    Add connection for presentations table

---
+++ GrpcService/Database/Presentations/Connection.cs
@@ -0,0 +1,13 @@
+﻿using GrpcService1.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrpcService1.Database.Presentations;
+
+public class Connection : GrpcService1.Database.Connection
+{
+    public DbSet<Presentation> Presentations;
+
+    public Connection(string connectionString) : base(connectionString)
+    {
+    }
+}
