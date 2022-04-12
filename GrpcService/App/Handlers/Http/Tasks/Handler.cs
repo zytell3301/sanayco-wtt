3523739348a8a9ea32acbd8bddf7f40fc89f7639
zytell3301 <zytel3301@gmail.com>
2022-04-12 13:44:06 +0430

    Add http handler RecordTask core method

--- GrpcService/App/Handlers/Http/Tasks/Handler.cs
+++ GrpcService/App/Handlers/Http/Tasks/Handler.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 namespace GrpcService1.App.Handlers.Http.tasks;
 
+[Route("/tasks")]
 public class Handler : ControllerBase
 {
     private Core.Tasks.Core Core;
@@ -9,5 +11,39 @@ public class Handler : ControllerBase
     public Handler(Core.Tasks.Core core)
     {
         Core = core;
+    }
+
+    [HttpPost("submit-task")]
+    public string RecordTask(IFormCollection formCollection)
+    {
+        formCollection.TryGetValue("description", out var Description);
+        formCollection.TryGetValue("project_id", out var DrojectId);
+        formCollection.TryGetValue("end_time", out var EndTime);
+        formCollection.TryGetValue("title", out var Ttitle);
+        formCollection.TryGetValue("work_location", out var WorkLocation);
+        switch (ModelState.IsValid)
+        {
+            case false:
+                // A proper error must be returned to the client
+                return "internal error";
+                break;
+        }
+
+        try
+        {
+            Core.RecordTask(new Domain.Entities.Task()
+            {
+                Description = Description,
+                Title = Ttitle,
+                EndTime = DateTime.UnixEpoch.AddSeconds(Int32.Parse(EndTime)),
+            });
+        }
+        catch (Exception e)
+        {
+            // @TODO A proper error must be returned to the client
+            return "internal error";
+        }
+
+        return "operation successful";
     }
 }
