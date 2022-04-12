843aadb48caa0006b2354880c26860685621c21c
zytell3301 <zytel3301@gmail.com>
2022-04-12 08:59:01 +0430

    Change method interface of GetOffTimeHistory in IDatabase

--- GrpcService/App/Core/OffTime/IDatabase.cs
+++ GrpcService/App/Core/OffTime/IDatabase.cs
@@ -9,6 +9,6 @@ namespace GrpcService1.App.Core.OffTime;
 public interface IDatabase
 {
     public void RecordOffTime(Domain.Entities.OffTime offTime);
-    public Domain.Entities.OffTime[] GetOffTimeHistory(User user, DateTime from, DateTime to);
+    public List<Domain.Entities.OffTime> GetOffTimeHistory(User user, DateTime @from, DateTime to);
     public void ChangeOffTimeStatus(Domain.Entities.OffTime offTime, string status);
 }
