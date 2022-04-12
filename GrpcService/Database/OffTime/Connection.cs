f718e86535fc952382e40e8c2e60dd116ef306b8
zytell3301 <zytel3301@gmail.com>
2022-04-12 08:26:44 +0430

    Add connection for OffTime database

---
+++ GrpcService/Database/OffTime/Connection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GrpcService1.Database.OffTime;
+
+public class Connection : GrpcService1.Database.Connection
+{
+    public DbSet<Domain.Entities.OffTime> OffTimes;
+
+    public Connection(string connectionString) : base(connectionString)
+    {
+    }
+}
