0a035d3f1b3f392027f57e5d0b7f27e6fbcc581b
zytell3301 <zytel3301@gmail.com>
2022-04-13 17:13:01 +0430

    Add http handler for RemoveMember method

--- GrpcService/App/Handlers/Http/Projects/Handler.cs
+++ GrpcService/App/Handlers/Http/Projects/Handler.cs
@@ -113,4 +113,36 @@ public class Handler : ControllerBase
 
         return "operation successful";
     }
+
+    public string RemoveMember([FromForm] int project_id, [FromForm] int user_id)
+    {
+        RemoveMemberValidation validation = new RemoveMemberValidation()
+        {
+            ProjectId = project_id,
+            UserId = user_id,
+        };
+
+        switch (ModelState.IsValid)
+        {
+            case false:
+                // @TODO A proper error must be returned for invalid data
+                return "data validation failed";
+        }
+
+        try
+        {
+            Core.RemoveMember(new ProjectMember()
+            {
+                ProjectId = validation.ProjectId,
+                UserId = validation.UserId,
+            });
+        }
+        catch (Exception e)
+        {
+            // @TODO A proper error must be returned to user for invalid data
+            return "Internal error occurred";
+        }
+
+        return "operation successful";
+    }
 }
