7225f80da44404e2bc9d02c2d643f1a5bfafb673
zytell3301 <zytel3301@gmail.com>
2022-04-13 09:28:52 +0430

    Implement production version of ErrorReporter

--- ErrorReporter/ErrorReporter.cs
+++ ErrorReporter/ErrorReporter.cs
@@ -1,13 +1,37 @@
-﻿namespace ErrorReporter;
+﻿using System.Net;
+using Confluent.Kafka;
+
+namespace ErrorReporter;
 
 public class Reporter : IErrorReporter
 {
-    /*
-     * This class is implemented for TESTING PURPOSE for now.
-     * @TODO Complete implementation of error reporter
-     */
+    private ProducerConfig Config;
+
+    public Reporter(string bootstrapServers)
+    {
+        Config = new ProducerConfig()
+        {
+            BootstrapServers = bootstrapServers,
+            ClientId = Dns.GetHostName(),
+        };
+    }
+
+    private IProducer<T, TS> BuildProducer<T, TS>()
+    {
+        return new ProducerBuilder<T, TS>(Config).Build();
+    }
+
     public void ReportException(Exception exception)
     {
-        Console.WriteLine(exception.Message);
+        Error.Error error = new Error.Error()
+        {
+            Message = exception.Message,
+            Code = (uint) exception
+                .GetHashCode(), // For now the code is the class hash code but in future releases it can be changed to anything (Even an string code)
+        };
+        BuildProducer<Null, string>().ProduceAsync("errors", new Message<Null, string>()
+        {
+            Value = error.ToString(), // Propagates error as protocol buffer encoded format to error recorder service
+        });
     }
 }
