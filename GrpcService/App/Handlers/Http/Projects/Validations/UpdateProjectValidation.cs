e59c9559dff221024a1200bc7bedc93bdde910de
zytell3301 <zytel3301@gmail.com>
2022-04-13 16:19:22 +0430

    Add validation for UpdateProject method

---
+++ GrpcService/App/Handlers/Http/Projects/Validations/UpdateProjectValidation.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Projects.Validations;
+
+public class UpdateProjectValidation
+{
+    [MaxLength(1024)] public string Name;
+    [MaxLength(32)] public string Description;
+}
