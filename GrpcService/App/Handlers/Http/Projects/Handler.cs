ca85954f751edff672a4c64f9dd56f019621e57c
zytell3301 <zytel3301@gmail.com>
2022-04-13 16:13:06 +0430

    Add http handler for RecordProject method

---
+++ GrpcService/App/Handlers/Http/Projects/Handler.cs
@@ -0,0 +1,52 @@
+﻿using GrpcService1.App.Handlers.Http.Projects.Validations;
+using GrpcService1.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcService1.App.Handlers.Http.Projects;
+
+[Route("/projects")]
+public class Handler : ControllerBase
+{
+    private Core.Projects.Core Core;
+
+    public Handler(Core.Projects.Core core)
+    {
+        Core = core;
+    }
+
+    [Route("new-project")]
+    public string RecordProject([FromForm] string description, [FromForm] string name, [FromForm] int user_id)
+    {
+        RecordProjectValidation validation = new RecordProjectValidation()
+        {
+            Description = description,
+            Name = name,
+            UserId = user_id,
+        };
+
+        switch (ModelState.IsValid)
+        {
+            case false:
+                // @TODO A proper message must be returned to user for invalid data
+                return "data validation failed";
+        }
+
+        try
+        {
+            Core.RecordProject(new Project()
+            {
+                Description = validation.Description,
+                Name = validation.Name,
+            }, new User()
+            {
+                Id = validation.UserId
+            });
+        }
+        catch (Exception e)
+        {
+            return "internal error";
+        }
+
+        return "operation successful";
+    }
+}
