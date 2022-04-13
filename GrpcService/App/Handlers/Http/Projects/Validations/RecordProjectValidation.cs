06bd0497640de4cd5663b772c1379e3ee480569d
zytell3301 <zytel3301@gmail.com>
2022-04-13 16:12:18 +0430

    Add validation rule for RecordProject method

---
+++ GrpcService/App/Handlers/Http/Projects/Validations/RecordProjectValidation.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Projects.Validations;
+
+public class RecordProjectValidation
+{
+    [MaxLength(1024)] public string Description;
+    [MaxLength(32)] public string Name;
+    [Required] public int UserId;
+}
