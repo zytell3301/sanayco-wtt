f33c43d1b377944ed952f43f541df2f63df891d6
zytell3301 <zytel3301@gmail.com>
2022-04-13 15:51:44 +0430

    Add validation for EditTask method

---
+++ GrpcService/App/Handlers/Http/Tasks/Validations/EditTaskValidation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.tasks.Validations;
+
+public class EditTaskValidation
+{
+    public int Id;
+    [MaxLength(1024)] public string Description;
+    [Timestamp] public int EndTime;
+    [MaxLength(32)] public string Title;
+    public string WorkLocation;
+    public int UserId;
+}
