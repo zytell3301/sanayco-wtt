e90d6ab2ad17390185c9dfe16b65b8a971171468
zytell3301 <zytel3301@gmail.com>
2022-04-13 09:36:08 +0430

    Add FakeReporter class for testing purposes

---
+++ ErrorReporter/FakeReporter.cs
@@ -0,0 +1,15 @@
+﻿namespace ErrorReporter;
+
+public class FakeReporter : IErrorReporter
+{
+    /*
+     * THIS CLASS IS ONLY IMPLEMENTED FOR TESTING PURPOSES.
+     * If the application is moving to production, the original
+     * reporter class must be uses or you will lose the control
+     * over all errors that are being happened in application.
+     */
+    public void ReportException(Exception exception)
+    {
+        Console.WriteLine(exception.Message);
+    }
+}
