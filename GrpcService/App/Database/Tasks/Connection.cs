93cee6567549b3e36fbbcbe258cd1f7a69bf9264
zytell3301 <zytel3301@gmail.com>
2022-04-13 11:14:15 +0430

    Fix a minor bug in Tasks connection

--- GrpcService/App/Database/Tasks/Connection.cs
+++ GrpcService/App/Database/Tasks/Connection.cs
@@ -4,7 +4,7 @@ namespace GrpcService1.App.Database.Tasks;
 
 public class Connection : Database.Connection
 {
-    public DbSet<Domain.Entities.Task> Tasks;
+    public DbSet<Domain.Entities.Task> Tasks { get; set; }
 
     public Connection(string connectionString) : base(connectionString)
     {
