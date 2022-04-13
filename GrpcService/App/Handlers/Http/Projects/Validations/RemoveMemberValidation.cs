dd5924874f3023bad2eae1c2a908d54e4c3ea0b0
zytell3301 <zytel3301@gmail.com>
2022-04-13 16:40:54 +0430

    Add validation for RemoveMember method

---
+++ GrpcService/App/Handlers/Http/Projects/Validations/RemoveMemberValidation.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Projects.Validations;
+
+public class RemoveMemberValidation
+{
+    [Required] public int UserId;
+    [Required] public int ProjectId;
+}
