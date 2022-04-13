add8eea99537a0af18e69dab62ceb4c2a1342467
zytell3301 <zytel3301@gmail.com>
2022-04-13 16:32:08 +0430

    Add validation for AddMember method

---
+++ GrpcService/App/Handlers/Http/Projects/Validations/AddMemberValidation.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Projects.Validations;
+
+public class AddMemberValidation
+{
+    [Required] public int ProjectId;
+    [Required] public int UserId;
+    [Required] public string Level;
+}
