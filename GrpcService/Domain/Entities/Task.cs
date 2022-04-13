85e3ceeba03febf027f837535f4764f026a8eebe
zytell3301 <zytel3301@gmail.com>
2022-04-13 12:07:03 +0430

    Remove database related attributes in tasks domain entity

--- GrpcService/Domain/Entities/Task.cs
+++ GrpcService/Domain/Entities/Task.cs
@@ -8,25 +8,19 @@ using System.Runtime.CompilerServices;
 
 namespace GrpcService1.Domain.Entities;
 
-[Table("tasks")]
 public class Task
 {
-    [Column("created_at")] public DateTime? CreatedAt { get; set; }
-    [Column("description")] public string Description { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string Description { get; set; }
 
-    [Column("end_time")]
-    [Required]
-    [Timestamp]
-    public DateTime EndTime { get; set; }
+    [Required] [Timestamp] public DateTime EndTime { get; set; }
 
-    [Column("id")] public int Id { get; set; }
-    [Column("project_id")] [Required] public int ProjectId { get; set; }
-    [Column("status")] public string Status { get; set; }
+    public int Id { get; set; }
+    [Required] public int ProjectId { get; set; }
+    public string Status { get; set; }
 
-    [Column("title")]
-    [Required, MaxLength(32)]
-    public string Title { get; set; }
+    [Required, MaxLength(32)] public string Title { get; set; }
 
-    [Column("work_location")] [Required] public string WorkLocation { get; set; }
-    [Column("user_id")] public int UserId { get; set; }
+    [Required] public string WorkLocation { get; set; }
+    public int UserId { get; set; }
 }
