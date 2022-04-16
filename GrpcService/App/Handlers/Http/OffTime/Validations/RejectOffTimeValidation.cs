64f438786a80713a134d5c1818901cd739d1d5c9
zytell3301 <zytel3301@gmail.com>
2022-04-16 09:27:48 +0430

    Add validation for RejectOffTime method

---
+++ GrpcService/App/Handlers/Http/OffTime/Validations/RejectOffTimeValidation.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.OffTime.Validations;
+
+public class RejectOffTimeValidation
+{
+    [Required] public int OffTimeId;
+}
