2c1f56162e06dc0eb62598dfde2540189afbfd26
zytell3301 <zytel3301@gmail.com>
2022-04-16 09:22:43 +0430

    Add validation for ApproveOffTime method

---
+++ GrpcService/App/Handlers/Http/OffTime/Validations/ApproveOffTimeValidation.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.OffTime.Validations;
+
+public class ApproveOffTimeValidation
+{
+    [Required] public int OffTimeId;
+}
