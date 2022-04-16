2f97df9ffc623da2060683fee2b17315e527fa30
zytell3301 <zytel3301@gmail.com>
2022-04-16 08:58:20 +0430

    Add base controller for off-time handler

---
+++ GrpcService/App/Handlers/Http/OffTime/Handler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcService1.App.Handlers.Http.OffTime;
+
+public class Handler : ControllerBase
+{
+    private Core.OffTime.Core Core;
+
+    public Handler(Core.OffTime.Core core)
+    {
+        Core = core;
+    }
+}
