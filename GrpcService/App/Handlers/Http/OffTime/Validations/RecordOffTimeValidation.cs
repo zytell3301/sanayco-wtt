570f9463e6f6d94fa45ff1f4db4477a9fe24d3b5
zytell3301 <zytel3301@gmail.com>
2022-04-16 09:17:12 +0430

    Add validation for RecordOffTime method

---
+++ GrpcService/App/Handlers/Http/OffTime/Validations/RecordOffTimeValidation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.OffTime.Validations;
+
+public class RecordOffTimeValidation
+{
+    [Required] public int FromDate;
+    [Required] public int ToDate;
+    [Required] public int UserId;
+    public string Description;
+}
