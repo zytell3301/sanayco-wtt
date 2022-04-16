4ea4f9bf4249c2d95602064997018a2ac0ecc370
zytell3301 <zytel3301@gmail.com>
2022-04-16 08:52:48 +0430

    Add validation for GetPresentationTime method

---
+++ GrpcService/App/Handlers/Http/Presentation/Validations/GetPresentationTimeValidation.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Presentation.Validations;
+
+public class GetPresentationTimeValidation
+{
+    [Required] public int UserId;
+}
