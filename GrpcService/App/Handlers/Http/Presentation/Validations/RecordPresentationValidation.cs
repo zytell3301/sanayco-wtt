cb0da7b12afc5525ef4c515a30753491414bbd0e
zytell3301 <zytel3301@gmail.com>
2022-04-16 08:33:02 +0430

    Add validation for RecordPresentation method

---
+++ GrpcService/App/Handlers/Http/Presentation/Validations/RecordPresentationValidation.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Presentation.Validations;
+
+public class RecordPresentationValidation
+{
+    [Required] public int UserId;
+}
