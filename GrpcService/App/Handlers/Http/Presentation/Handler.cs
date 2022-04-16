169858bd74c96f5bcc471e6c8598099d74ba0674
zytell3301 <zytel3301@gmail.com>
2022-04-16 08:35:47 +0430

    Add http handler for RecordPresentation core method

--- GrpcService/App/Handlers/Http/Presentation/Handler.cs
+++ GrpcService/App/Handlers/Http/Presentation/Handler.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GrpcService1.App.Handlers.Http.Presentation.Validations;
+using GrpcService1.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GrpcService1.App.Handlers.Http.Presentation;
 
@@ -9,5 +11,35 @@ public class Handler : ControllerBase
     public Handler(Core.Presentation.Core core)
     {
         Core = core;
+    }
+
+    public string RecordPresentation([FromForm] int user_id)
+    {
+        RecordPresentationValidation validation = new RecordPresentationValidation()
+        {
+            UserId = user_id,
+        };
+
+        switch (ModelState.IsValid)
+        {
+            case false:
+                // @TODO A proper error must be returned because of invalid data
+                return "data validation failed";
+        }
+
+        try
+        {
+            Core.RecordPresentation(new User()
+            {
+                Id = validation.UserId,
+            });
+        }
+        catch (Exception e)
+        {
+            // @TODO A proper error must be returned  to user for internal failure
+            return "operation failed";
+        }
+
+        return "operation successful";
     }
 }
