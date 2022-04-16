86a423f54539c8edb67817557802b9e5c3b26c70
zytell3301 <zytel3301@gmail.com>
2022-04-16 08:29:19 +0430

    Add base controller for presentation core

---
+++ GrpcService/App/Handlers/Http/Presentation/Handler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcService1.App.Handlers.Http.Presentation;
+
+public class Handler : ControllerBase
+{
+    private Core.Presentation.Core Core;
+
+    public Handler(Core.Presentation.Core core)
+    {
+        Core = core;
+    }
+}
