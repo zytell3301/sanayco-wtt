684ea2dc993571031a3a626426e7927c3cd34822
zytell3301 <zytel3301@gmail.com>
2022-04-16 08:21:32 +0430

    Add validation for DeleteProject method

---
+++ GrpcService/App/Handlers/Http/Projects/Validations/DeleteProjectValidation.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Projects.Validations;
+
+public class DeleteMemberValidation
+{
+    [Required] public int ProjectId;
+}
