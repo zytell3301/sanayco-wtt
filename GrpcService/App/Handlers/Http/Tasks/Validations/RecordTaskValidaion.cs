b191f2ee8ad135df58746bb91473becf2ed8b943
zytell3301 <zytel3301@gmail.com>
2022-04-17 16:07:44 +0430

    Add validation for RecordTask method

---
+++ GrpcService/App/Handlers/Http/Tasks/Validations/RecordTaskValidaion.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.tasks.Validations;
+
+public class RecordTaskValidaion
+{
+    public string description { get; set; }
+    [MaxLength(32)] public string title { get; set; }
+    public int end_time { get; set; }
+    public int project_id { get; set; }
+    public string work_location { get; set; }
+    public int user_id { get; set; }
+}
