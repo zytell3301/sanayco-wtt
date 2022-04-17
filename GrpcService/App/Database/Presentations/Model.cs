ee0187a1281ec962b049f7eab8b44f123cbf61eb
zytell3301 <zytel3301@gmail.com>
2022-04-17 13:29:43 +0430

    Add model for presentation database

---
+++ GrpcService/App/Database/Presentations/Model.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GrpcService1.App.Database.Presentations;
+
+public class Model
+{
+    [Column("id")] public int Id;
+    [Column("user_id")] public int UserId;
+    [Column("start")] public DateTime Start;
+    [Column("end")] public DateTime End;
+}
