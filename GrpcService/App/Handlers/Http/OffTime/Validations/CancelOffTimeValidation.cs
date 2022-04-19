3a02a152fc22bf3a08ce9163d4e4e86878a536e5
zytell3301 <zytel3301@gmail.com>
2022-04-19 13:19:49 +0430

    Add CancelOffTimeValidation class

---
+++ GrpcService/App/Handlers/Http/OffTime/Validations/CancelOffTimeValidation.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.OffTime.Validations;
+
+public class CancelOffTimeValidation
+{
+    [Required] public int off_time_id { get; set; }
+}
