83a3cebe6bb64694fd8fa2b2120819b3c8132471
zytell3301 <zytel3301@gmail.com>
2022-04-19 15:53:11 +0430

    Add EditOffTimeValidation validation class

---
+++ GrpcService/App/Handlers/Http/OffTime/Validations/EditOffTimeValidation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.OffTime.Validations;
+
+public class EditOffTimeValidation
+{
+    [Required] public int off_time_id { get; set; }
+    [MaxLength(1024)] public string description { get; set; }
+    public int from_date { get; set; }
+    public int to_date { get; set; }
+}
