de9c1642a9296424e3b403da8aba0eae524eac89
zytell3301 <zytel3301@gmail.com>
2022-04-19 09:33:29 +0430

    Add InvalidRequestResponse constant to base handler

--- GrpcService/App/Handlers/Http/BaseHandler.cs
+++ GrpcService/App/Handlers/Http/BaseHandler.cs
@@ -5,6 +5,9 @@ namespace GrpcService1.App.Handlers.Http;
 
 public class BaseHandler : ControllerBase
 {
+    // This is the response that will be returned if the client is sending invalid data
+    protected const string InvalidRequestResponse = "";
+
     private string ParsePayload()
     {
         return new StreamReader(Request.BodyReader.AsStream()).ReadToEnd();
