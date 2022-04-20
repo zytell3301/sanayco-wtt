ece205e8fd46c2c9d725d760b7a7e74486234884
zytell3301 <zytel3301@gmail.com>
2022-04-20 10:14:57 +0430

    Increase field lenght of level in project members table

--- GrpcService/DatabaseSchema/004_CREATE_TABLE_PROJECT_MEMBERS.sql
+++ GrpcService/DatabaseSchema/004_CREATE_TABLE_PROJECT_MEMBERS.sql
@@ -3,7 +3,7 @@ CREATE TABLE project_members
     id         INT IDENTITY (1,1),
     user_id    INT,
     project_id INT,
-    level      CHAR(16),
+    level      CHAR(32),
     created_at DATETIME,
     PRIMARY KEY (id),
 )
