67a5281fd7747ce2ad039f3bce469ce18ea662ae
zytell3301 <zytel3301@gmail.com>
2022-04-20 16:18:14 +0430

    Implement base class of TokenGenerator

---
+++ GrpcService/App/TokenGenerator/TokenGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace GrpcService1.App.TokenGenerator;
+
+public class TokenGenerator
+{
+    private readonly int TokenLength;
+    private readonly string ValidCharacters;
+
+    public class TokenGeneratorConfigs
+    {
+        public int TokenLength;
+        public string ValidaCharacters;
+    }
+
+    public TokenGenerator(TokenGeneratorConfigs configs)
+    {
+        TokenLength = configs.TokenLength;
+        ValidCharacters = configs.ValidaCharacters;
+    }
+}
