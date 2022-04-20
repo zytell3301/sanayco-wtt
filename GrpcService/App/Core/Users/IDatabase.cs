133a1a34c8fd223e0537e9db2b4ee68459b18be0
zytell3301 <zytel3301@gmail.com>
2022-04-20 15:03:03 +0430

    Add RecordUser method to users cores database

--- GrpcService/App/Core/Users/IDatabase.cs
+++ GrpcService/App/Core/Users/IDatabase.cs
@@ -6,4 +6,5 @@ public interface IDatabase
 {
     public User GetUser(User user);
     public void RecordToken(Domain.Entities.Token token);
+    public void RecordUser(Domain.Entities.User user);
 }
