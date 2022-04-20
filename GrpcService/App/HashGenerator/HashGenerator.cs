50869dadda03092beacda028de04a3324d1bc8fd
zytell3301 <zytel3301@gmail.com>
2022-04-20 15:36:04 +0430

    Implement base of HashGenerator class

---
+++ GrpcService/App/HashGenerator/HashGenerator.cs
@@ -0,0 +1,38 @@
+﻿using ErrorReporter;
+using GrpcService1.App.Core.Users;
+
+namespace GrpcService1.App.HashGenerator;
+
+public class HashGenerator : IHash
+{
+    private readonly int HashCost;
+
+    private IErrorReporter ErrorReporter;
+
+    public class HashGeneratorConfigs
+    {
+        public int HashCost;
+    }
+
+    public class HashGeneratorDependencies
+    {
+        public IErrorReporter ErrorReporter;
+    }
+
+    public HashGenerator(HashGeneratorConfigs configs, HashGeneratorDependencies dependencies)
+    {
+        ErrorReporter = dependencies.ErrorReporter;
+
+        HashCost = configs.HashCost;
+    }
+
+    public string Hash(string expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool VerifyHash(string hash, string original)
+    {
+        throw new NotImplementedException();
+    }
+}
