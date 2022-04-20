d5e1bfbec99d4c12abc165a9156da179f91dcbc5
zytell3301 <zytel3301@gmail.com>
2022-04-20 14:53:31 +0430

    Add token domain entity

---
+++ GrpcService/Domain/Entities/Token.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcService1.Domain.Entities;
+
+public class Token
+{
+    public int Id { get; set; }
+    public int? UserId { get; set; }
+    public string? Token1 { get; set; }
+    public DateTime? ExpirationDate { get; set; }
+    public DateTime? CreatedAt { get; set; }
+}
