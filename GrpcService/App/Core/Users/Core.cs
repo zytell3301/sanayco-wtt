a0074dd06d8630042d545a01c7f2408abd38e4a6
zytell3301 <zytel3301@gmail.com>
2022-04-20 13:48:55 +0430

    Initiate users core class

---
+++ GrpcService/App/Core/Users/Core.cs
@@ -0,0 +1,22 @@
+﻿using GrpcService1.Domain.Entities;
+
+namespace GrpcService1.App.Core.Users;
+
+public class Core
+{
+    private IDatabase Database;
+
+    public class UsersCoreConfigs
+    {
+    }
+
+    public class UsersCoreDependencies
+    {
+        public IDatabase Database;
+    }
+
+    public Core(UsersCoreConfigs configs, UsersCoreDependencies dependencies)
+    {
+        Database = dependencies.Database;
+    }
+}
