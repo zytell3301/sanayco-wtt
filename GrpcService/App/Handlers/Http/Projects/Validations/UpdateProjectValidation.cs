cf60e8a1fe8d73f59d1f25037168f7a2598afb75
zytell3301 <zytel3301@gmail.com>
2022-04-20 10:39:15 +0430

    Make UpdateProjectValidation json friendly

--- GrpcService/App/Handlers/Http/Projects/Validations/UpdateProjectValidation.cs
+++ GrpcService/App/Handlers/Http/Projects/Validations/UpdateProjectValidation.cs
@@ -4,6 +4,7 @@ namespace GrpcService1.App.Handlers.Http.Projects.Validations;
 
 public class UpdateProjectValidation
 {
-    [MaxLength(1024)] public string Name;
-    [MaxLength(32)] public string Description;
+    [MaxLength(1024)] public string name { get; set; }
+    [MaxLength(32)] public string description { get; set; }
+    [Required] public int project_id { get; set; }
 }
