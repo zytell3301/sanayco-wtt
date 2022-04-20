37f45ef5a1ff6b43279c3acfcf51a64ac74b82f1
zytell3301 <zytel3301@gmail.com>
2022-04-20 14:55:07 +0430

    Implement users core Login method

--- GrpcService/App/Core/Users/Core.cs
+++ GrpcService/App/Core/Users/Core.cs
@@ -12,10 +12,14 @@ public class Core
     private readonly InvalidCredentials InvalidCredentials;
     private readonly InternalError InternalError;
 
+    private readonly int ExpirationWindow;
+
     public class UsersCoreConfigs
     {
         public string InvalidCredentialsMessage;
         public string InternalErrorMessage;
+
+        public int ExpirationWindow;
     }
 
     public class UsersCoreDependencies
@@ -33,5 +37,37 @@ public class Core
 
         InvalidCredentials = new InvalidCredentials(configs.InvalidCredentialsMessage);
         InternalError = new InternalError(configs.InternalErrorMessage);
+
+        ExpirationWindow = configs.ExpirationWindow;
+    }
+
+    public Domain.Entities.Token Login(User user, string password)
+    {
+        try
+        {
+            user = Database.GetUser(user);
+            switch (Hash.VerifyHash(user.Password, password))
+            {
+                // Password hashes does not match. So the password is incorrect.
+                // @TODO Number of wrong attempts must be recorded for every user to prevent from brute force attacks.
+                case false:
+                    throw InvalidCredentials;
+            }
+
+            var token = new Token()
+            {
+                Token1 = TokenGenerator.GenerateToken(),
+                UserId = user.Id,
+                ExpirationDate = DateTime.Now.AddSeconds(ExpirationWindow),
+            };
+
+            Database.RecordToken(token);
+
+            return token;
+        }
+        catch (Exception e)
+        {
+            throw InternalError;
+        }
     }
 }
