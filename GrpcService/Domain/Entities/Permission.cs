744f7ecfe41e39f2600a7b29687fe447f1564480
zytell3301 <zytel3301@gmail.com>
2022-04-24 14:24:01 +0430

    Add permission domain entity

---
+++ GrpcService/Domain/Entities/Permission.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcService1.Domain.Entities;
+
+public class Permission
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Title { get; set; }
+    public int GrantedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
