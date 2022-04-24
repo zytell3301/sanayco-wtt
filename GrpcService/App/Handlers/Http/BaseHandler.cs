a71c6f70a1e343d5b1a0c04175ff08567a02f7bc
zytell3301 <zytel3301@gmail.com>
2022-04-24 14:31:20 +0430

    Add Authorization method to BaseHandler

--- GrpcService/App/Handlers/Http/BaseHandler.cs
+++ GrpcService/App/Handlers/Http/BaseHandler.cs
@@ -13,12 +13,18 @@ public class BaseHandler : ControllerBase
     // This is the response that will be returned if the client is sending invalid data
     protected const string InvalidRequestResponse = "";
     private readonly AuthenticationFailed AuthenticationFailed;
+    private readonly AuthorizationFailed AuthorizationFailed;
     private readonly ITokenSource TokenSource;
+    private IPermissionsSource PermissionsSource;
 
-    public BaseHandler(ITokenSource tokenSource, AuthenticationFailed authenticationFailed)
+    public BaseHandler(ITokenSource tokenSource, IPermissionsSource permissionsSource,
+        AuthenticationFailed authenticationFailed,
+        AuthorizationFailed authorizationFailed)
     {
         TokenSource = tokenSource;
         AuthenticationFailed = authenticationFailed;
+        AuthorizationFailed = authorizationFailed;
+        PermissionsSource = permissionsSource;
     }
 
     private string ParsePayload()
@@ -40,6 +46,29 @@ public class BaseHandler : ControllerBase
         catch (Exception e)
         {
             throw AuthenticationFailed;
+        }
+    }
+
+    protected int Authorize(string permission)
+    {
+        try
+        {
+            int userId = Authenticate();
+            switch (PermissionsSource.CheckPermission(userId, permission))
+            {
+                case false:
+                    throw AuthorizationFailed;
+            }
+
+            return userId;
+        }
+        catch (AuthenticationFailed e)
+        {
+            throw AuthenticationFailed;
+        }
+        catch (Exception e)
+        {
+            throw AuthorizationFailed;
         }
     }
 }
