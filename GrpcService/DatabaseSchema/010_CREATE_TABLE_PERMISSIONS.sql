45ed5badec328260e13f12adeae8a3f49970c015
zytell3301 <zytel3301@gmail.com>
2022-04-24 14:04:56 +0430

    Add permissions table to database

---
+++ GrpcService/DatabaseSchema/010_CREATE_TABLE_PERMISSIONS.sql
@@ -0,0 +1,15 @@
+/* It is recommended to log every permission. Maybe we want trace that who granted the permission to current user*/
+CREATE TABLE permissions
+(
+    id         INT IDENTITY (1,1),
+    user_id    INT, /* The id of the user that has the permission*/
+    title      VARCHAR(32),
+    granted_by INT, /* The if of the user that granted the permission */
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+);
+
+ALTER TABLE permissions
+    ADD CONSTRAINT FK_PERMISSIONS_USERS_USER_ID_ID FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE permissions
+    ADD CONSTRAINT FK_PERMISSIONS_USERS_GRANTED_BY_ID FOREIGN KEY (granted_by) REFERENCES users (id) ON DELETE NO ACTION;
