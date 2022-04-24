47e7e46ae71c137d324697c3c042ce235e2faac4
zytell3301 <zytel3301@gmail.com>
2022-04-24 14:24:24 +0430

    Add IPermissionsSource interface

---
+++ GrpcService/App/Handlers/Http/IPermissionsSource.cs
@@ -0,0 +1,8 @@
+﻿using GrpcService1.App.Database.Model;
+
+namespace GrpcService1.App.Handlers.Http;
+
+public interface IPermissionsSource
+{
+    public bool CheckPermission(int userId, string permission);
+}
