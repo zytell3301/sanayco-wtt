2253de7046aa51345269c86fa57e11b1f7e79734
zytell3301 <zytel3301@gmail.com>
2022-04-24 14:55:30 +0430

    Add BaseHandlerDependencies class

---
+++ GrpcService/App/Handlers/Http/BaseHandlerDependencies.cs
@@ -0,0 +1,11 @@
+﻿using GrpcService1.Domain.Errors;
+
+namespace GrpcService1.App.Handlers.Http;
+
+public class BaseHandlerDependencies
+{
+    public ITokenSource TokenSource { get; set; }
+    public IPermissionsSource PermissionsSource { get; set; }
+    public AuthenticationFailed AuthenticationFailed { get; set; }
+    public AuthorizationFailed AuthorizationFailed { get; set; }
+}
