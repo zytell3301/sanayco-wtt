71da1c826ca4c68564b40eb75d47ec47e0d22adc
zytell3301 <zytel3301@gmail.com>
2022-04-24 12:40:39 +0430

    Implement ITokenSource interface

---
+++ GrpcService/App/TokenSource/TokenSource.cs
@@ -0,0 +1,19 @@
+﻿using GrpcService1.App.Database.Model;
+using GrpcService1.App.Handlers.Http;
+
+namespace GrpcService1.App.TokenSource;
+
+public class TokenSource : ITokenSource
+{
+    private wttContext Connection;
+
+    public TokenSource(Database.Model.wttContext connection)
+    {
+        Connection = connection;
+    }
+
+    public int GetTokenUserId(string token)
+    {
+        return Connection.Tokens.Where(token1 => token1.Token1 == token).First().UserId.Value;
+    }
+}
