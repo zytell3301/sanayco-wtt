340921873b9d93fe20bca3eae92e93f9e5fce9ee
zytell3301 <zytel3301@gmail.com>
2022-04-24 12:59:00 +0430

    Implement Authenticate method in BaseHandler

--- GrpcService/App/Handlers/Http/BaseHandler.cs
+++ GrpcService/App/Handlers/Http/BaseHandler.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System.Text.Json;
+using GrpcService1.Domain.Errors;
 using Microsoft.AspNetCore.Mvc;
 
 #endregion
@@ -9,6 +10,15 @@ namespace GrpcService1.App.Handlers.Http;
 
 public class BaseHandler : ControllerBase
 {
+    private ITokenSource TokenSource;
+    private AuthenticationFailed AuthenticationFailed;
+
+    public BaseHandler(ITokenSource tokenSource, AuthenticationFailed authenticationFailed)
+    {
+        TokenSource = tokenSource;
+        AuthenticationFailed = authenticationFailed;
+    }
+
     // This is the response that will be returned if the client is sending invalid data
     protected const string InvalidRequestResponse = "";
 
@@ -20,5 +30,17 @@ public class BaseHandler : ControllerBase
     protected T DecodePayloadJson<T>()
     {
         return JsonSerializer.Deserialize<T>(ParsePayload());
+    }
+
+    protected int Authenticate()
+    {
+        try
+        {
+            return TokenSource.GetTokenUserId(Request.Headers["auth-token"]);
+        }
+        catch (Exception e)
+        {
+            throw AuthenticationFailed;
+        }
     }
 }
