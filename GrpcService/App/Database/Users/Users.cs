c0fac81754d4675f112ace37906693726fad3a58
zytell3301 <zytel3301@gmail.com>
2022-04-25 09:20:02 +0430

    Implement users database GetUser method

--- GrpcService/App/Database/Users/Users.cs
+++ GrpcService/App/Database/Users/Users.cs
@@ -1,5 +1,7 @@
-﻿using GrpcService1.App.Core.Users;
+﻿using ErrorReporter;
+using GrpcService1.App.Core.Users;
 using GrpcService1.App.Database.Model;
+using GrpcService1.Domain.Errors;
 using Token = GrpcService1.Domain.Entities.Token;
 using User = GrpcService1.Domain.Entities.User;
 
@@ -8,15 +10,36 @@ namespace GrpcService1.App.Database.Users;
 public class Users : IDatabase
 {
     private wttContext Connection;
+    private IErrorReporter ErrorReporter;
+    private readonly InternalError InternalError;
 
-    public Users(wttContext connection)
+    public class UsersDatabaseDependencies
     {
-        Connection = connection;
+        public wttContext Connection;
+        public IErrorReporter ErrorReporter;
+
+        public InternalError InternalError;
+    }
+
+    public Users(UsersDatabaseDependencies dependencies)
+    {
+        Connection = dependencies.Connection;
+        ErrorReporter = dependencies.ErrorReporter;
+
+        InternalError = dependencies.InternalError;
     }
 
     public User GetUser(User user)
     {
-        throw new NotImplementedException();
+        try
+        {
+            return ConvertModelToUser(Connection.Users.First(u => u.Id == user.Id));
+        }
+        catch (Exception e)
+        {
+            ErrorReporter.ReportException(e);
+            throw InternalError;
+        }
     }
 
     public void RecordToken(Token token)
@@ -32,5 +55,27 @@ public class Users : IDatabase
     public User GetUserByUsername(User user)
     {
         throw new NotImplementedException();
+    }
+
+    private Domain.Entities.User ConvertModelToUser(Model.User model)
+    {
+        var user = new Domain.Entities.User()
+        {
+            Id = model.Id,
+            Name = model.Name,
+            Password = model.Password,
+            CompanyLevel = model.CompanyLevel,
+            LastName = model.Lastname,
+            SkillLevel = model.SkillLevel,
+        };
+
+        switch (model.CreatedAt.HasValue)
+        {
+            case true:
+                user.CreatedAt = model.CreatedAt.Value;
+                break;
+        }
+
+        return user;
     }
 }
