8638670e6d2cb0d4cdfb44d1d4e9a845fcb37e56
zytell3301 <zytel3301@gmail.com>
2022-04-25 09:12:07 +0430

    Add base class for Users database class

---
+++ GrpcService/App/Database/Users/Users.cs
@@ -0,0 +1,36 @@
+﻿using GrpcService1.App.Core.Users;
+using GrpcService1.App.Database.Model;
+using Token = GrpcService1.Domain.Entities.Token;
+using User = GrpcService1.Domain.Entities.User;
+
+namespace GrpcService1.App.Database.Users;
+
+public class Users : IDatabase
+{
+    private wttContext Connection;
+
+    public Users(wttContext connection)
+    {
+        Connection = connection;
+    }
+
+    public User GetUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RecordToken(Token token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RecordUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User GetUserByUsername(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
