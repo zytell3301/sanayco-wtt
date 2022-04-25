addab5bd1026392a9c58d6d9fe6eba4c0e5fb486
zytell3301 <zytel3301@gmail.com>
2022-04-25 10:59:37 +0430

    Add get set accessors to Login validation fields

--- GrpcService/App/Handlers/Http/Users/Validations/LoginValidation.cs
+++ GrpcService/App/Handlers/Http/Users/Validations/LoginValidation.cs
@@ -4,9 +4,13 @@ namespace GrpcService1.App.Handlers.Http.Users.Validations;
 
 public class LoginValidation
 {
-    [Required] [MinLength(8)] [MaxLength(32)]
-    public string username;
+    [Required]
+    [MinLength(8)]
+    [MaxLength(32)]
+    public string username { get; set; }
 
-    [Required] [MinLength(8)] [MaxLength(32)]
-    public string password;
+    [Required]
+    [MinLength(8)]
+    [MaxLength(32)]
+    public string password { get; set; }
 }
