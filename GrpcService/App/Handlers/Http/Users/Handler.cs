61253c28dbadc88aa0a657c023b05cfe4f4fd576
zytell3301 <zytel3301@gmail.com>
2022-04-25 09:09:00 +0430

    Add base class for users handler

---
+++ GrpcService/App/Handlers/Http/Users/Handler.cs
@@ -0,0 +1,12 @@
+﻿namespace GrpcService1.App.Handlers.Http.Users;
+
+public class Handler : BaseHandler
+{
+    private Core.Users.Core Core;
+
+    public Handler(Core.Users.Core core, BaseHandlerDependencies baseHandlerDependencies) : base(
+        baseHandlerDependencies)
+    {
+        Core = core;
+    }
+}
