75c66062797814bf04bb1f4b5e14d8b2593a8519
zytell3301 <zytel3301@gmail.com>
2022-04-25 10:09:38 +0430

    Implement http handler for cores Login method

--- GrpcService/App/Handlers/Http/Users/Handler.cs
+++ GrpcService/App/Handlers/Http/Users/Handler.cs
@@ -1,4 +1,9 @@
-﻿namespace GrpcService1.App.Handlers.Http.Users;
+﻿using System.Text.Json;
+using GrpcService1.App.Handlers.Http.Users.Validations;
+using GrpcService1.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcService1.App.Handlers.Http.Users;
 
 public class Handler : BaseHandler
 {
@@ -8,5 +13,51 @@ public class Handler : BaseHandler
         baseHandlerDependencies)
     {
         Core = core;
+    }
+
+    private class LoginResponse : Response
+    {
+        public string token { get; set; }
+        public DateTime expiration_date { get; set; }
+    }
+
+    [HttpPost("/login")]
+    public string Login()
+    {
+        LoginValidation body;
+        try
+        {
+            body = DecodePayloadJson<LoginValidation>();
+        }
+        catch (Exception e)
+        {
+            // @TODO Since this exception happens when client is sending invalid data, we can store  current request data for ip blacklist analysis
+            return InvalidRequestResponse;
+        }
+
+        switch (ModelState.IsValid)
+        {
+            case false:
+                return ResponseToJson(DataValidationFailedResponse());
+        }
+
+        try
+        {
+            var token = Core.Login(new User()
+            {
+                Username = body.username,
+            }, body.password);
+
+            return JsonSerializer.Serialize(new LoginResponse()
+            {
+                status_code = 0,
+                token = token.Token1,
+                expiration_date = token.ExpirationDate.Value,
+            });
+        }
+        catch (Exception e)
+        {
+            return ResponseToJson(InternalErrorResponse());
+        }
     }
 }
