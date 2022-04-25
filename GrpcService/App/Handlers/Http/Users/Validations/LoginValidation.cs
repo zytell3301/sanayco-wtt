2ae7dd44d02672c63202e022b5c6b781b1ec056c
zytell3301 <zytel3301@gmail.com>
2022-04-25 10:08:29 +0430

    Add validation for Login method

---
+++ GrpcService/App/Handlers/Http/Users/Validations/LoginValidation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Users.Validations;
+
+public class LoginValidation
+{
+    [Required] [MinLength(8)] [MaxLength(32)]
+    public string username;
+
+    [Required] [MinLength(8)] [MaxLength(32)]
+    public string password;
+}
