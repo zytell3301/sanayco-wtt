8772b2d3753a02b6fdf38ef4a098d1ce93d369c6
zytell3301 <zytel3301@gmail.com>
2022-04-26 16:26:31 +0430

    Add mission domain entity

---
+++ GrpcService/Domain/Entities/Mission.cs
@@ -0,0 +1,11 @@
+﻿namespace GrpcService1.Domain.Entities;
+
+public class Mission
+{
+    public int Id { get; set; }
+    public int MemberId { get; set; }
+    public int ProjectId { get; set; }
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+    public string Description { get; set; }
+}
