7bd19255443c4d0e681afbb25c28fd17960a2722
zytell3301 <zytel3301@gmail.com>
2022-04-26 16:22:35 +0430

    Add missions database table schema

---
+++ GrpcService/DatabaseSchema/012_CREATE_TABLE_MISSIONS.sql
@@ -0,0 +1,17 @@
+USE wtt;
+
+CREATE TABLE missions
+(
+    id          INT IDENTITY (1,1) PRIMARY KEY,
+    member_id   INT      NOT NULL,
+    project_id  INT      NOT NULL,
+    from_date   DATETIME NOT NULL,
+    to_date     DATETIME NOT NULL,
+    description VARCHAR  NOT NULL,
+);
+
+ALTER TABLE missions
+    ADD CONSTRAINT FK_MISSIONS_MEMBER_ID_ID FOREIGN KEY (member_id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE
+
+ALTER TABLE missions
+    ADD CONSTRAINT FK_MISSIONS_PROJECT_ID_ID FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE ON UPDATE CASCADE;
