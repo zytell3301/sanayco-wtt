c184664f30a6016491457a860fff9ef101d3ee96
zytell3301 <zytel3301@gmail.com>
2022-04-26 09:04:37 +0430

    Implement RecordPermissionsBatch database interface

---
+++ GrpcService/App/Database/Users/RecordPermissionsBatch.cs
@@ -0,0 +1,45 @@
+﻿using GrpcService1.App.Core.Users;
+using GrpcService1.App.Database.Model;
+using GrpcService1.Domain.Errors;
+using Permission = GrpcService1.Domain.Entities.Permission;
+
+namespace GrpcService1.App.Database.Users;
+
+public class RecordPermissionsBatch : IRecordPermissionsBatch
+{
+    private wttContext Connection;
+
+    public RecordPermissionsBatch(wttContext connection)
+    {
+        Connection = connection;
+    }
+
+    public void AddPermissionToBatch(Permission permission)
+    {
+        try
+        {
+            Connection.Permissions.Add(new Model.Permission()
+            {
+                Title = permission.Title,
+                GrantedBy = permission.GrantedBy,
+                UserId = permission.UserId,
+            });
+        }
+        catch (Exception)
+        {
+            throw new InternalError("internal error occurred");
+        }
+    }
+
+    public void SaveChanges()
+    {
+        try
+        {
+            Connection.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            throw new InternalError("internal error occurred");
+        }
+    }
+}
