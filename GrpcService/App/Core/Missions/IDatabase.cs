3b39d599a3d518be5f0af508239946e557ce568a
zytell3301 <zytel3301@gmail.com>
2022-04-26 16:58:14 +0430

    Add GetMission to database interface

--- GrpcService/App/Core/Missions/IDatabase.cs
+++ GrpcService/App/Core/Missions/IDatabase.cs
@@ -5,4 +5,5 @@ public interface IDatabase
     public void RecordMission(Domain.Entities.Mission mission);
     public void DeleteMission(Domain.Entities.Mission mission);
     public void UpdateMission(Domain.Entities.Mission mission);
+    public Domain.Entities.Mission GetMission(Domain.Entities.Mission mission);
 }
