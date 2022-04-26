5a5d5f3920db16273ea439aa4c1a2097b7bb306f
zytell3301 <zytel3301@gmail.com>
2022-04-26 14:59:47 +0430

    Add UpdateUsersValidation validation class

---
+++ GrpcService/App/Handlers/Http/Users/Validations/UpdateUserValidation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Users.Validations;
+
+public class UpdateUserValidation
+{
+    [Required] public int user_id { get; set; }
+    public string name { get; set; }
+    public string lastname { get; set; }
+    public string username { get; set; }
+    public string skill_level { get; set; }
+    public string company_level { get; set; }
+    public string[] permissions { get; set; }
+}
