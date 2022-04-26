9d00415410e690da3bfc0657c8a80b8e4906167c
zytell3301 <zytel3301@gmail.com>
2022-04-26 09:06:12 +0430

    Add RecordUserValidation validation class

---
+++ GrpcService/App/Handlers/Http/Users/Validations/RecordUserValidation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Users.Validations;
+
+public class RecordUserValidation
+{
+    [Required] public string name { get; set; }
+    [Required] public string lastname { get; set; }
+    [Required] public string skill_level { get; set; }
+    [Required] public string company_level { get; set; }
+    [Required] public string password { get; set; }
+    [Required] public string username { get; set; }
+    [Required] public string[] permissions { get; set; }
+}
