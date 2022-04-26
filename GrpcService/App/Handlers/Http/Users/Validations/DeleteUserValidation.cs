2144d32aa97de818a0dfb7aa4f600a4f05965f46
zytell3301 <zytel3301@gmail.com>
2022-04-26 09:43:24 +0430

    Add DeleteUserValidation validation class

---
+++ GrpcService/App/Handlers/Http/Users/Validations/DeleteUserValidation.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Users.Validations;
+
+public class DeleteUserValidation
+{
+    [Required] public string username { get; set; }
+}
