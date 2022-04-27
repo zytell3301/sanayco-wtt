5d2eb146fecd41f34cf637ffd646c4453c079189
zytell3301 <zytel3301@gmail.com>
2022-04-27 08:51:53 +0430

    Update mission domain entity due to recent database changes

--- GrpcService/Domain/Entities/Mission.cs
+++ GrpcService/Domain/Entities/Mission.cs
@@ -8,4 +8,7 @@ public class Mission
     public DateTime FromDate { get; set; }
     public DateTime ToDate { get; set; }
     public string Description { get; set; }
+    public string Title { get; set; }
+    public string Location { get; set; }
+    public bool IsVerified { get; set; }
 }
