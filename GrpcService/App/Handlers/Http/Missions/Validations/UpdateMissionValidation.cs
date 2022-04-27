e3fa650006e03b61affdeff2f3775da10ce863cd
zytell3301 <zytel3301@gmail.com>
2022-04-27 10:09:32 +0430

    Add UpdateMissionValidation validation class

---
+++ GrpcService/App/Handlers/Http/Missions/Validations/UpdateMissionValidation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Missions.Validations;
+
+public class UpdateMissionValidation
+{
+    [Required] public int mission_id;
+    public string description;
+    public int project_id;
+    public int from_date;
+    public int to_date;
+    public string title;
+    public string location;
+}
