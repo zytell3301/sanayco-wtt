32bf546840e209eeb6ede32b8f08715abd868eb1
zytell3301 <zytel3301@gmail.com>
2022-04-27 09:05:18 +0430

    Add RecordMissionValidation validation class

---
+++ GrpcService/App/Handlers/Http/Missions/Validations/RecordMissionsValidation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrpcService1.App.Handlers.Http.Missions.Validations;
+
+public class RecordMissionsValidation
+{
+    [Required] public int project_id;
+    [Required] public int from_date;
+    [Required] public int to_date;
+    [Required] public string description;
+    [Required] public string title;
+    [Required] public string location;
+    [Required] public bool is_verified;
+}
