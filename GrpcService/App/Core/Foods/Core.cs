eeeb8e6a63daf23e643f9c49614177d6b282a6a1
zytell3301 <zytel3301@gmail.com>
2022-04-27 15:43:46 +0430

    Add base class for foods core

---
+++ GrpcService/App/Core/Foods/Core.cs
@@ -0,0 +1,27 @@
+﻿using GrpcService1.Domain.Errors;
+
+namespace GrpcService1.App.Core.Foods;
+
+public class Core
+{
+    private readonly InternalError InternalError;
+
+    private IDatabase Database;
+
+    public class FoodsCoreDependencies
+    {
+        public IDatabase Database;
+    }
+
+    public class FoodsCoreConfigs
+    {
+        public string InternalErrorMessage;
+    }
+
+    public Core(FoodsCoreDependencies dependencies, FoodsCoreConfigs configs)
+    {
+        InternalError = new InternalError(configs.InternalErrorMessage);
+
+        Database = dependencies.Database;
+    }
+}
