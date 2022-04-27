4f19f0f564dce4e3ac7a9783fe80413d9772a77a
zytell3301 <zytel3301@gmail.com>
2022-04-27 15:46:26 +0430

    Add food related entites

---
+++ GrpcService/Domain/Entities/Food.cs
@@ -0,0 +1,9 @@
+﻿namespace GrpcService1.Domain.Entities;
+
+public class Food
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Price { get; set; }
+    public bool IsAvailable { get; set; }
+}
---
+++ GrpcService/Domain/Entities/FoodOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcService1.Domain.Entities;
+
+public class FoodOrder
+{
+    public int Id { get; set; }
+    public int FoodId { get; set; }
+    public int UserId { get; set; }
+    public int Price { get; set; }
+    public DateTime Date { get; set; }
+}
