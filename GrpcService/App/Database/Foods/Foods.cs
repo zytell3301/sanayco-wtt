bc817419de08ee4ed84fda263715bf483c7f538a
zytell3301 <zytel3301@gmail.com>
2022-04-27 16:16:27 +0430

    Implement RecordFood database method

--- GrpcService/App/Database/Foods/Foods.cs
+++ GrpcService/App/Database/Foods/Foods.cs
@@ -1,6 +1,7 @@
 ﻿using ErrorReporter;
 using GrpcService1.App.Core.Foods;
 using GrpcService1.App.Database.Model;
+using GrpcService1.Domain.Errors;
 using Food = GrpcService1.Domain.Entities.Food;
 using FoodOrder = GrpcService1.Domain.Entities.FoodOrder;
 
@@ -10,16 +11,33 @@ public class Foods : IDatabase
 {
     private wttContext Connection;
     private IErrorReporter ErrorReporter;
+    private readonly InternalError InternalError;
 
-    public Foods(wttContext connection, IErrorReporter errorReporter)
+    public Foods(wttContext connection, IErrorReporter errorReporter, InternalError internalError)
     {
         Connection = connection;
         ErrorReporter = errorReporter;
+
+        InternalError = internalError;
     }
 
     public void RecordFood(Food food)
     {
-        throw new NotImplementedException();
+        try
+        {
+            Connection.Foods.Add(new Model.Food()
+            {
+                Price = food.Price,
+                Title = food.Title,
+                IsAvailable = food.IsAvailable,
+            });
+            Connection.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            ErrorReporter.ReportException(e);
+            throw InternalError;
+        }
     }
 
     public void UpdateFoodInfo(Food food)
