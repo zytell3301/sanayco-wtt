7921d1eb19b4f491f9b5a060ef6e8d157f0551b5
zytell3301 <zytel3301@gmail.com>
2022-04-27 09:06:08 +0430

    Add untried http handler for RecordMission core method

--- GrpcService/App/Handlers/Http/Missions/Handler.cs
+++ GrpcService/App/Handlers/Http/Missions/Handler.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GrpcService1.App.Handlers.Http.Missions.Validations;
+using GrpcService1.Domain.Entities;
+using GrpcService1.Domain.Errors;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GrpcService1.App.Handlers.Http.Missions;
 
@@ -11,5 +14,56 @@ public class Handler : BaseHandler
         baseHandlerDependencies)
     {
         Core = core;
+    }
+
+    [HttpPost("record-missions")]
+    public string RecordMission()
+    {
+        try
+        {
+            Authorize("mission");
+        }
+        catch (Exception)
+        {
+            return ResponseToJson(AuthenticationFailedResponse());
+        }
+
+        RecordMissionsValidation body;
+        try
+        {
+            body = DecodePayloadJson<RecordMissionsValidation>();
+        }
+        catch (Exception)
+        {
+            // @TODO Since this exception happens when client is sending invalid data, we can store  current request data for ip blacklist analysis
+            return InvalidRequestResponse;
+        }
+
+        switch (ModelState.IsValid)
+        {
+            case false:
+                return ResponseToJson(DataValidationFailedResponse());
+        }
+
+        try
+        {
+            Core.RecordMission(new Mission()
+            {
+                Location = body.location,
+                Description = body.description,
+                Title = body.title,
+                FromDate = DateTime.UnixEpoch.AddSeconds(body.from_date),
+                IsVerified = body.is_verified,
+                MemberId = GetUserId(),
+                ToDate = DateTime.UnixEpoch.AddSeconds(body.to_date),
+                ProjectId = body.project_id,
+            });
+        }
+        catch (Exception)
+        {
+            return ResponseToJson(InternalErrorResponse());
+        }
+
+        return ResponseToJson(OperationSuccessfulResponse());
     }
 }
