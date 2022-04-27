13f83ba0960b440d1a4563ea3fa2af4bad1af360
zytell3301 <zytel3301@gmail.com>
2022-04-27 08:51:08 +0430

    Add some missing fields to missions table

---
+++ GrpcService/DatabaseSchema/013_ALTER_TABLE_MISSIONS.sql
@@ -0,0 +1,8 @@
+USE wtt;
+
+ALTER TABLE missions
+    ADD title VARCHAR(128) NOT NULL;
+ALTER TABLE missions
+    ADD location VARCHAR(128) NOT NULL;
+ALTER TABLE missions
+    ADD is_verified BIT NOT NULL DEFAULT 0;
