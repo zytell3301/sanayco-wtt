7780d2a2b797ac662d88c26c34295ffbd3564184
zytell3301 <zytel3301@gmail.com>
2022-04-27 09:41:21 +0430

    Add http handler for DeleteMission core method

--- GrpcService/App/Handlers/Http/Missions/Handler.cs
+++ GrpcService/App/Handlers/Http/Missions/Handler.cs
@@ -1,4 +1,5 @@
 ﻿using GrpcService1.App.Handlers.Http.Missions.Validations;
+using GrpcService1.App.Handlers.Http.Users.Validations;
 using GrpcService1.Domain.Entities;
 using GrpcService1.Domain.Errors;
 using Microsoft.AspNetCore.Mvc;
@@ -57,6 +58,50 @@ public class Handler : BaseHandler
                 MemberId = GetUserId(),
                 ToDate = DateTime.UnixEpoch.AddSeconds(body.to_date),
                 ProjectId = body.project_id,
+            });
+        }
+        catch (Exception)
+        {
+            return ResponseToJson(InternalErrorResponse());
+        }
+
+        return ResponseToJson(OperationSuccessfulResponse());
+    }
+
+    [HttpPost("delete-mission")]
+    public string DeleteMission()
+    {
+        try
+        {
+            // @TODO We must check that if the requesting client is the owner of the current entity
+        }
+        catch (Exception)
+        {
+            return ResponseToJson(AuthorizationFailedResponse());
+        }
+
+        DeleteMissionValidation body;
+        try
+        {
+            body = DecodePayloadJson<DeleteMissionValidation>();
+        }
+        catch (Exception)
+        {
+            // @TODO Since this exception happens when client is sending invalid data, we can store  current request data for ip blacklist analysis
+            return InvalidRequestResponse;
+        }
+
+        switch (ModelState.IsValid)
+        {
+            case false:
+                return ResponseToJson(DataValidationFailedResponse());
+        }
+
+        try
+        {
+            Core.DeleteMission(new Mission()
+            {
+                Id = body.mission_id,
             });
         }
         catch (Exception)
