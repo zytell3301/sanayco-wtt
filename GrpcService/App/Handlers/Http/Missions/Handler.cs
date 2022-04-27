15c331e0334b9e7324931d4e21b59d5dfa78fcf5
zytell3301 <zytel3301@gmail.com>
2022-04-27 08:26:59 +0430

    Add base handler class for missions core

---
+++ GrpcService/App/Handlers/Http/Missions/Handler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcService1.App.Handlers.Http.Missions;
+
+[Route("/missions")]
+public class Handler : BaseHandler
+{
+    private Core.Missions.Core Core;
+
+    public Handler(Core.Missions.Core core, BaseHandlerDependencies baseHandlerDependencies) : base(
+        baseHandlerDependencies)
+    {
+        Core = core;
+    }
+}
