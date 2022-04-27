8d1957adf4fe6e720e967406926f3bc3594a5388
zytell3301 <zytel3301@gmail.com>
2022-04-27 16:13:55 +0430

    Add base class for foods database

---
+++ GrpcService/App/Database/Foods/Foods.cs
@@ -0,0 +1,54 @@
+﻿using ErrorReporter;
+using GrpcService1.App.Core.Foods;
+using GrpcService1.App.Database.Model;
+using Food = GrpcService1.Domain.Entities.Food;
+using FoodOrder = GrpcService1.Domain.Entities.FoodOrder;
+
+namespace GrpcService1.App.Database.Foods;
+
+public class Foods : IDatabase
+{
+    private wttContext Connection;
+    private IErrorReporter ErrorReporter;
+
+    public Foods(wttContext connection, IErrorReporter errorReporter)
+    {
+        Connection = connection;
+        ErrorReporter = errorReporter;
+    }
+
+    public void RecordFood(Food food)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateFoodInfo(Food food)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Food GetFoodInfo(Food food)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChangeFoodStatus(Food food)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteFood(Food food)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RecordOrder(FoodOrder order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteOrder(FoodOrder order)
+    {
+        throw new NotImplementedException();
+    }
+}
