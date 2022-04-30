81c27bc596e4a42bc09b1e1e62191f5bdc254b60
zytell3301 <zytel3301@gmail.com>
2022-04-30 08:42:58 +0430

    Add base handler class for Foods handler

---
+++ GrpcService/App/Handlers/Http/Foods/Handler.cs
@@ -0,0 +1,12 @@
+﻿namespace GrpcService1.App.Handlers.Http.Foods;
+
+public class Handler : BaseHandler
+{
+    private Core.Foods.Core Core;
+
+    public Handler(Core.Foods.Core core, BaseHandlerDependencies baseHandlerDependencies) : base(
+        baseHandlerDependencies)
+    {
+        Core = core;
+    }
+}
