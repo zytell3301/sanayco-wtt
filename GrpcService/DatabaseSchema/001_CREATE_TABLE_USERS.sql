9a4073ebaf13cc47144c3ee60119038ac8133bcf
zytell3301 <zytel3301@gmail.com>
2022-05-07 08:33:05 +0430

    Change field length of skill_level and company_level of users table

--- GrpcService/DatabaseSchema/001_CREATE_TABLE_USERS.sql
+++ GrpcService/DatabaseSchema/001_CREATE_TABLE_USERS.sql
@@ -5,8 +5,8 @@ CREATE TABLE users
     id            INT IDENTITY (1,1),
     name          VARCHAR(32),
     lastname      VARCHAR(32),
-    skill_level   VARCHAR(10), /* Set this to a lower value if needed*/
-    company_level VARCHAR(10),
+    skill_level   VARCHAR(32), /* Set this to a lower value if needed*/
+    company_level VARCHAR(32),
     created_at    DATETIME,
     PRIMARY KEY (id),
 )
