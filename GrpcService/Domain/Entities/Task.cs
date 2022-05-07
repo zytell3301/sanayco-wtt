362216424046f3a49d4723bd431952a182d37fab
zytell3301 <zytel3301@gmail.com>
2022-05-07 09:42:06 +0430

    Remove validation rules from Task domain entity

--- GrpcService/Domain/Entities/Task.cs
+++ GrpcService/Domain/Entities/Task.cs
@@ -11,14 +11,14 @@ public class Task
     public DateTime? CreatedAt { get; set; }
     public string Description { get; set; }
 
-    [Required] [Timestamp] public DateTime EndTime { get; set; }
+    public DateTime EndTime { get; set; }
 
     public int Id { get; set; }
     [Required] public int ProjectId { get; set; }
     public string Status { get; set; }
 
-    [Required] [MaxLength(32)] public string Title { get; set; }
+    public string Title { get; set; }
 
-    [Required] public string WorkLocation { get; set; }
+    public string WorkLocation { get; set; }
     public int UserId { get; set; }
 }
