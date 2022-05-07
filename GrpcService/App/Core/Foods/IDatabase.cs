70e7f4d19fa384b18abf3711ea07730b8229f6c8
zytell3301 <zytel3301@gmail.com>
2022-05-07 15:30:16 +0430

    Add GetAvailableFoodsList method to foods core database interface

--- GrpcService/App/Core/Foods/IDatabase.cs
+++ GrpcService/App/Core/Foods/IDatabase.cs
@@ -16,4 +16,5 @@ public interface IDatabase
     public void RecordOrder(FoodOrder order);
     public void DeleteOrder(FoodOrder order);
     public FoodOrder GetOrder(FoodOrder order);
+    public List<Domain.Entities.Food> GetAvailableFoodsList();
 }
