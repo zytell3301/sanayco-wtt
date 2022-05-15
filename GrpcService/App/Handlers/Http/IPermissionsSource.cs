1d0caac023a87db625acaa07bca39896a5197d9c
zytell3301 <zytel3301@gmail.com>
2022-05-15 12:22:03 +0430

    Add GetUserPermissions method to IPermissionsSource interface and implement it

--- GrpcService/App/Handlers/Http/IPermissionsSource.cs
+++ GrpcService/App/Handlers/Http/IPermissionsSource.cs
@@ -1,6 +1,9 @@
-﻿namespace GrpcService1.App.Handlers.Http;
+﻿using GrpcService1.Domain.Entities;
+
+namespace GrpcService1.App.Handlers.Http;
 
 public interface IPermissionsSource
 {
     public bool CheckPermission(int userId, string permission);
+    public List<Permission> GetUserPermissions(int userId);
 }
--- GrpcService/App/PermissionsSource/PermissionsSource.cs
+++ GrpcService/App/PermissionsSource/PermissionsSource.cs
@@ -2,6 +2,8 @@
 
 using GrpcService1.App.Database.Model;
 using GrpcService1.App.Handlers.Http;
+using GrpcService1.Domain.Errors;
+using Permission = GrpcService1.Domain.Entities.Permission;
 
 #endregion
 
@@ -20,7 +22,7 @@ public class PermissionsSource : IPermissionsSource
     {
         try
         {
-            Connection.Permissions.Where(p => p.UserId == userId).First(p => p.Title == permission);
+            GetUserPermissionQuery(userId).First(p => p.Title == permission);
             return
                 true; // If the code reaches here it means that the previous line didn't throw a record not found exception
         }
@@ -28,5 +30,40 @@ public class PermissionsSource : IPermissionsSource
         {
             return false;
         }
+    }
+
+    private IQueryable<Database.Model.Permission> GetUserPermissionQuery(int userId)
+    {
+        return Connection.Permissions.Where(p => p.UserId == userId);
+    }
+
+    public List<Permission> GetUserPermissions(int userId)
+    {
+        var permissions = new List<Permission>();
+        try
+        {
+            foreach (var permission in GetUserPermissionQuery(userId).ToList())
+            {
+                permissions.Add(ConvertPermissionModel(permission));
+            }
+
+            return permissions;
+        }
+        catch (Exception)
+        {
+            throw new InternalError("");
+        }
+    }
+
+    private Permission ConvertPermissionModel(Database.Model.Permission model)
+    {
+        return new Permission()
+        {
+            Id = model.Id,
+            Title = model.Title,
+            CreatedAt = model.CreatedAt.Value,
+            GrantedBy = model.GrantedBy.Value,
+            UserId = model.UserId.Value,
+        };
     }
 }
