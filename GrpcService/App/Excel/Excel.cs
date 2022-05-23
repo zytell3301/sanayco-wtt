8920a396279dab37630e24b2a6484388a19e7bd6
zytell3301 <zytel3301@gmail.com>
2022-05-23 10:39:28 +0430

    Fix thread unsafty on excel report production

--- GrpcService/App/Excel/Excel.cs
+++ GrpcService/App/Excel/Excel.cs
@@ -3,33 +3,41 @@ using OfficeOpenXml;
 
 namespace GrpcService1.App.Excel;
 
-public class Excel : IExcel, IExcel.IExcelFile, IExcel.IExcelManager
+public class Excel : IExcel
 {
-    private ExcelPackage ExcelPackage;
-    private ExcelWorksheet ExcelWorksheet;
-    private MemoryStream Stream;
-
     public IExcel.IExcelManager NewExcel()
     {
-        ExcelPackage = new ExcelPackage();
-        ExcelWorksheet = ExcelPackage.Workbook.Worksheets.Add("Sheet 1");
-        Stream = new MemoryStream();
-        return this;
+
+        return new ExcelManager();
     }
 
-    public byte[] GetByte()
+    public class ExcelManager : IExcel.IExcelFile,IExcel.IExcelManager
     {
-        return Stream.GetBuffer();
-    }
+        private ExcelPackage ExcelPackage;
+        private ExcelWorksheet ExcelWorksheet;
+        private MemoryStream Stream;
 
-    public void SetCell(int line, int column, string value)
-    {
-        ExcelWorksheet.Cells[line, column].Value = value;
-    }
+        public ExcelManager()
+        {
+            ExcelPackage = new ExcelPackage();
+            ExcelWorksheet = ExcelPackage.Workbook.Worksheets.Add("Sheet 1");
+            Stream = new MemoryStream();
+        }
+        
+        public byte[] GetByte()
+        {
+            return Stream.GetBuffer();
+        }
 
-    public IExcel.IExcelFile GetExcelFile()
-    {
-        ExcelPackage.SaveAs(Stream);
-        return this;
+        public void SetCell(int line, int column, string value)
+        {
+            ExcelWorksheet.Cells[line, column].Value = value;
+        }
+
+        public IExcel.IExcelFile GetExcelFile()
+        {
+            ExcelPackage.SaveAs(Stream);
+            return this;
+        }   
     }
 }
